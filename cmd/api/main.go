b46a0e2458e2133fc577e8a65d88ebdc1012a4a9
Oussama Moutafatin <moutafatin3@gmail.com>
2023-12-03 16:45:25 +0000

    hello word http server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello there")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+
+		w.Write([]byte("Hello there"))
+	})
+
+	log.Println("server running on http://localhost:4000")
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
