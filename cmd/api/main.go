91936663c65a3499c010d80fc690a3134751a808
Oussama Moutafatin <moutafatin3@gmail.com>
2023-12-03 20:27:41 +0000

    little refactor and connect to database using pgx

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,62 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/subosito/gotenv"
 )
 
-func main() {
-	srv := &http.Server{
-		Addr:    ":4000",
-		Handler: routes(),
-	}
+type dbConfig struct {
+	dsn string
+}
 
-	log.Println("server running on http://localhost:4000")
-	err := srv.ListenAndServe()
+type config struct {
+	port int
+	db   dbConfig
+}
+
+func main() {
+	gotenv.Load()
+	var cfg config
+
+	flag.StringVar(&cfg.db.dsn, "dsn", os.Getenv("POSTGRES_URL"), "Postgres connection url")
+	flag.IntVar(&cfg.port, "port", 4000, "TCP port to listen to")
+
+	db, err := openDb(cfg.db.dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	defer db.Close()
+	log.Println("Connected to database")
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.port),
+		Handler: routes(),
+	}
+
+	log.Println(fmt.Sprintf("server running on http://localhost%s", srv.Addr))
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func openDb(dsn string) (*pgxpool.Pool, error) {
+	db, err := pgxpool.New(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
