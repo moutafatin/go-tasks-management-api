49d7f488b00a4127e2ee5d0f6cb1c3400e092798
Oussama Moutafatin <moutafatin3@gmail.com>
2023-12-03 20:18:18 +0000

    create tasks migrations and a little script to manage migrations

---
+++ migrations/000001_create_tasks_table.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS tasks;
+
+DROP TYPE IF EXISTS task_status;
+
+DROP TYPE IF EXISTS task_priority;
---
+++ migrations/000001_create_tasks_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TYPE task_status AS ENUM ('TODO', 'IN_PROGRESS', 'DONE');
+
+CREATE TYPE task_priority AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+CREATE TABLE
+  IF NOT EXISTS tasks (
+    id Serial PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    status task_status NOT NULL DEFAULT 'TODO',
+    priority task_priority NOT NULL DEFAULT 'LOW',
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  )
